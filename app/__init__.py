c7b89ef6d6452d4085e9d8d2de5e797e85f5c395
Aakanksha NS <aakanksha.ns@gmail.com>
2020-04-22 19:58:49 -0700

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-
-
+from flask_cors import CORS, cross_origin
 
 
 # Initialization
 application = Flask(__name__)
+CORS(application, support_credentials=True)
 application.config.from_object(Config)
 
 DB_URI = application.config['SQLALCHEMY_DATABASE_URI']
