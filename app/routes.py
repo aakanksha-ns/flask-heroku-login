51408df75c6aba736db0c6006847de4e5021b7b6
Aakanksha NS <aakanksha.ns@gmail.com>
2020-05-06 00:10:25 -0700

    added price

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.orm import Session
 from flask_cors import CORS, cross_origin
 
-CORS(application,support_credentials=True)
+CORS(application, support_credentials=True)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Accounts = Base.classes.account
@@ -21,7 +21,7 @@ def index():
     return 'Welcome to this page'
 
 
-@application.route('/register', methods=["GET","POST"])
+@application.route('/register', methods=["GET", "POST"])
 def register():
     username = request.args.get('username')
     email = request.args.get('email')
@@ -33,7 +33,7 @@ def register():
     return jsonify({'user_added': True})
 
 
-@application.route('/sign_in', methods=["GET","POST"])
+@application.route('/sign_in', methods=["GET", "POST"])
 def sign_in():
     username_entered = request.args.get('username')
     password_entered = request.args.get('password')
@@ -44,21 +44,22 @@ def sign_in():
     return jsonify({'signed_in': False})
 
 
-@application.route('/add_book', methods=["GET","POST"])
+@application.route('/add_book', methods=["GET", "POST"])
 def add_book():
     isbn = request.args.get('isbn')
     book_title = request.args.get('book_title')
     book_author = request.args.get('book_author')
     publication_year = request.args.get('publication_year')
     image_url = request.args.get('image_url')
+    price = request.args.get('price')
     books = Table('books', metadata, autoload=True)
     engine.execute(books.insert(), isbn=isbn,
                    book_title=book_title, book_author=book_author, publication_year=publication_year,
-                   image_url=image_url)
+                   image_url=image_url, price=price)
     return jsonify({'book_added': True})
 
 
-@application.route('/fetch_books', methods=["GET","POST"])
+@application.route('/fetch_books', methods=["GET", "POST"])
 def fetch_books():
     books = session.query(Books).all()
     books_list = []
@@ -68,6 +69,7 @@ def fetch_books():
             'book_title': book.book_title,
             'book_author': book.book_author,
             'publication_year': book.publication_year,
-            'image_url': book.image_url
+            'image_url': book.image_url,
+            'price': book.price
         })
     return jsonify(books_list)
